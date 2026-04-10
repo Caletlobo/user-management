c619a9eddfac2177d277585cdfdb7df579bee95e
Calet <your-email@example.com>
2026-04-10 11:08:57 +0530

    Updated

--- server.js
+++ server.js
@@ -200,7 +200,7 @@ app.get('/users/paginate', async (req, res) => {
 // For local development
 if (process.env.NODE_ENV !== 'production') {
   const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Server running on port ${PORT}`);
   });
 }
